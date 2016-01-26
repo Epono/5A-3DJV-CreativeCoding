3bc8bee3741f2fdcf526f5f11654c90d10060101
Epono <guillaume.ambrois@gmail.com>
2016-01-26 15:56:04 +0100

    Des ptits cubes

--- js/env.js
+++
@@ -1,6 +0,0 @@
-var SCENE = null;
-var CAMERA = null;
-var RENDERER = null;
-var OBJECT = null;
-var KEYBOARD = null;
-var CANVAS = null;
--- js/main.js
+++ js/main.js
@@ -1,72 +1,192 @@
-var mousePos;
+var SCENE = null;
+var CAMERA = null;
+var RENDERER = null;
+var OBJECT = null;
+var KEYBOARD = null;
+var CANVAS = null;
 
-function init()
-{
+var mouseSpeed = 0.02;
+var mouse = {
+    pos: {
+        x: 0,
+        y: 0
+    },
+    deltaPos: {
+        x: 0,
+        y: 0
+    },
+    leftDown: false,
+    middleButton: false,
+    rightDown: false
+}
+
+function init() {
     SCENE = new THREE.Scene();
-    
-    CAMERA = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-    
+
+    CAMERA = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
     RENDERER = new THREE.WebGLRenderer();
     RENDERER.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(RENDERER.domElement);
-    
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    OBJECT = new THREE.Mesh( geometry, material );
-	
-    SCENE.add( OBJECT );
-    
+
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({
+        color: 0x00ff00
+    });
+    OBJECT = new THREE.Mesh(geometry, material);
+
+    SCENE.add(OBJECT);
+
     CAMERA.position.z = 5;
 
     KEYBOARD = new THREEx.KeyboardState();
 
-    console.log(RENDERER);
-
     CANVAS = RENDERER.domElement;
 
-    CANVAS.addEventListener('mousemove', function(evt) {
+    // MOUSE
+    CANVAS.addEventListener('mousemove', function (evt) {
         var rect = CANVAS.getBoundingClientRect();
-        mousePos = {
-          x : evt.clientX - rect.left,
-          y : evt.clientY - rect.top
+
+        var oldMousePos = mouse.pos;
+
+        mouse.pos = {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
         };
-        //console.log(mousePos);
+
+        if (mouse.leftDown || mouse.middleButton || mouse.rightDown) {
+            mouse.deltaPos = {
+                x: mouse.pos.x - oldMousePos.x,
+                y: mouse.pos.y - oldMousePos.y,
+            }
+        }
     }, false);
 
-    CANVAS.addEventListener('mousedown', function(evt) {
-        console.log(evt);
+    CANVAS.addEventListener('mousedown', function (evt) {
+        switch (event.button) {
+        case 0:
+            mouse.leftDown = true;
+            break;
+        case 1:
+            mouse.middleButton = true;
+            break;
+        case 2:
+            mouse.rightDown = true;
+            break;
+        }
     }, false);
 
-    CANVAS.addEventListener('mouseup', function(evt) {
-        console.log(evt);
+    CANVAS.addEventListener('mouseup', function (evt) {
+        switch (event.button) {
+        case 0:
+            mouse.leftDown = false;
+            break;
+        case 1:
+            mouse.middleButton = false;
+            break;
+        case 2:
+            mouse.rightDown = false;
+            break;
+        }
     }, false);
 }
 
-function render()
-{
-    if( KEYBOARD.pressed("z") ) 
-    {
-        console.log('lol');
+function render() {
+    //// UPDATE
+    // KEYBOARD
+    if (KEYBOARD.pressed("a") || KEYBOARD.pressed("z") || KEYBOARD.pressed("e") || KEYBOARD.pressed("r") || KEYBOARD.pressed("t") || KEYBOARD.pressed("y") || KEYBOARD.pressed("u") || KEYBOARD.pressed("i") || KEYBOARD.pressed("o") || KEYBOARD.pressed("p") || KEYBOARD.pressed("q") || KEYBOARD.pressed("s") || KEYBOARD.pressed("d") || KEYBOARD.pressed("f") || KEYBOARD.pressed("g") || KEYBOARD.pressed("h") || KEYBOARD.pressed("j") || KEYBOARD.pressed("k") || KEYBOARD.pressed("l") || KEYBOARD.pressed("m") || KEYBOARD.pressed("w") || KEYBOARD.pressed("x") || KEYBOARD.pressed("c") || KEYBOARD.pressed("v") || KEYBOARD.pressed("b") || KEYBOARD.pressed("n")) {
+        //console.log(KEYBOARD.keyCodes);
+
+        var geometry = new THREE.BoxGeometry(1, 1, 1);
+        var material = new THREE.MeshBasicMaterial({
+            color: 0x00ff00
+        });
+        var obj = new THREE.Mesh(geometry, material);
+
+        var nombreMagique = 0;
+        for (var i = 0; i < 26; i++) {
+            if (KEYBOARD.keyCodes[65 + i]) {
+                nombreMagique += Math.pow(2, i);
+            }
+        }
+
+        Math.seedrandom(nombreMagique);
+        console.log(Math.random());
+        console.log(Math.random());
+        console.log(Math.random());
+        console.log(Math.random());
+
+        var radius = 10;
+        obj.position.x = (Math.random() - 0.5) * radius;
+        obj.position.y = (Math.random() - 0.5) * radius;
+
+        // TODO: Faire toutes ces modifs en fonction du nombre magique, et pas du random
+        obj.material.color.r = Math.random();
+        obj.material.color.g = Math.random();
+        obj.material.color.b = Math.random();
+
+        obj.scale.x = Math.random();
+        obj.scale.y = Math.random();
+        obj.scale.z = Math.random();
+
+        obj.rotation.x = Math.random();
+        obj.rotation.y = Math.random();
+        obj.rotation.z = Math.random();
+
+        SCENE.add(obj);
+
+        setTimeout(function () {
+            SCENE.remove(obj);
+
+        }, 1000);
     }
 
-    requestAnimationFrame( render );
+    // MOUSE
+    if (mouse.leftDown) {
+        OBJECT.position.x += mouse.deltaPos.x * mouseSpeed;
+        OBJECT.position.y -= mouse.deltaPos.y * mouseSpeed;
+    }
 
+    // OTHER
     OBJECT.rotation.x += 0.05;
     OBJECT.rotation.y += 0.05;
 
-    RENDERER.render(SCENE, CAMERA);   
+    // RENDER
+    requestAnimationFrame(render);
+
+    RENDERER.render(SCENE, CAMERA);
 }
 
-$(document).ready(function() 
-{
+$(document).ready(function () {
     init();
     render();
 });
 
 
-      function getMousePos(canvas, evt) {
-        var rect = canvas.getBoundingClientRect();
-        return 
-      }
 
+var CustomRandom = function (nseed) {
 
+    var seed,
+        constant = Math.pow(2, 13) + 1,
+        prime = 37,
+        maximum = Math.pow(2, 50);
+
+    if (nseed) {
+        seed = nseed;
+    }
+
+    if (seed == null) {
+        //if there is no seed, use timestamp
+        seed = (new Date()).getTime();
+    }
+
+    return {
+        next: function () {
+            seed *= constant;
+            seed += prime;
+            seed %= maximum;
+
+            return seed;
+        }
+    }
+}
