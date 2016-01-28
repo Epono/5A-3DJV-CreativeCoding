b36bc4f04865a0f9bd8394c794e404ab1d92a08b
Epono <guillaume.ambrois@gmail.com>
2016-01-28 19:03:08 +0100

    cémieu

--- js/main.js
+++ js/main.js
@@ -18,7 +18,6 @@ javascript: (function () {
 var SCENE = null;
 var CAMERA = null;
 var RENDERER = null;
-var OBJECT = null;
 var CANVAS = null;
 var CONTROLS = null;
 
@@ -67,14 +66,12 @@ var uniforms = {
     }
 };
 
-var OBJECT2 = null;
+var OBJ_SPHERE = null;
+var OBJ_SKYBOX = null;
 
-
-
-var SKYBOX = null;
-var OBJECTS = [];
-var CIRCLEOBJECTS1 = [];
-var CIRCLEOBJECTS2 = [];
+var OBJECTS_FRONT = [];
+var OBJECTS_MID = [];
+var OBJECTS_BACK = [];
 
 var mySound = null;
 var mySoundAnalyser = null;
@@ -97,7 +94,7 @@ function preload() {
     sounds['B'].setVolume(0.1);
 
     sounds['F'] = loadSound('assets/sounds/F - batard.mp3');
-    sounds['F'].setVolume(0.1);
+    sounds['F'].setVolume(1);
 
     sounds['H'] = loadSound('assets/sounds/H - HINHINHIN.mp3');
     sounds['H'].setVolume(0.1);
@@ -112,7 +109,7 @@ function preload() {
     sounds['N'].setVolume(0.2);
 
     sounds['S'] = loadSound('assets/sounds/S - You got completely shitfaced.wav');
-    sounds['S'].setVolume(0.2);
+    sounds['S'].setVolume(1);
 
     // Pour cacher le cnnvas processing
     $('#p5_loading').hide();
@@ -127,147 +124,95 @@ function setup() {
 var date = Date.now();
 //////////////////////////////////////////////////////////////////////////////////// INIT SCENE
 function init() {
-
-    var geometry = new THREE.BoxGeometry(0.1, 0.2, 0.1);
-    var geometry2 = new THREE.BoxGeometry(0.05, 0.1, 0.1);
-    var geometry3 = new THREE.BoxGeometry(0.025, 0.05, 0.1);
-
     SCENE = new THREE.Scene();
 
     CAMERA = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    CAMERA.position.z = 5;
 
     RENDERER = new THREE.WebGLRenderer();
     RENDERER.setSize(window.innerWidth, window.innerHeight);
+    RENDERER.setClearColor(0x000000);
     document.body.appendChild(RENDERER.domElement);
 
-
-
-    CAMERA.position.z = 5;
-
     CANVAS = RENDERER.domElement;
-
-    RENDERER.setClearColor(0xfff000);
 
     CONTROLS = new THREE.OrbitControls(CAMERA, RENDERER.domElement);
     CONTROLS.enableDamping = true;
     CONTROLS.dampingFactor = 0.25;
 
 
-
-
-
-
-
-
-
-    //var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.ShaderMaterial({
-        uniforms: {
-            Rcolor: {
-                type: 'f',
-                value: 0.6
-            },
-            Gcolor: {
-                type: 'f',
-                value: 0.1
-            },
-            Bcolor: {
-                type: 'f',
-                value: 0.3
-            },
-            Alpha: {
-                type: 'f',
-                value: 1.0
-            }
-        },
-        vertexShader: document.getElementById("simple.vs").textContent,
-        fragmentShader: document.getElementById("simple.fs").textContent
-
-
-    });
-    OBJECT = new THREE.Mesh(geometry, material);
-
-
-    //var geometrySkybox = new THREE.BoxGeometry(10,10,10);
-    var geometrySkybox = new THREE.PlaneGeometry(20, 20);
-    var materialSkybox = new THREE.MeshBasicMaterial({
-        color: 0xffffff
-    });
-
-
-    //MY TEST ZONE
-
-    var geometryObj = new THREE.SphereGeometry(1, 32, 32);
+    // MATERIALS
     var materialObj = new THREE.ShaderMaterial({
         uniforms: uniforms,
         vertexShader: document.getElementById("simple.vs").textContent,
         fragmentShader: document.getElementById("simple.fs").textContent
-
-
     });
 
-    OBJECT2 = new THREE.Mesh(geometryObj, materialObj);
 
-    SKYBOX = new THREE.Mesh(geometrySkybox, materialSkybox);
+    var materialSkybox = new THREE.MeshBasicMaterial({
+        color: 0xfff000,
+        side: THREE.DoubleSide
+    });
 
+    // GEOMETRIES
+    var geometrySphere = new THREE.SphereGeometry(1, 32, 32);
+    var geometrySkybox = new THREE.BoxGeometry(100, 100, 100);
 
+    var geometryObjFront = new THREE.BoxGeometry(0.1, 0.2, 0.1);
+    var geometryObjMid = new THREE.BoxGeometry(0.05, 0.1, 0.1);
+    var geometryObjBack = new THREE.BoxGeometry(0.025, 0.05, 0.1);
 
-    //JON TEST
-
+    // CERCLES D'OBJETS
     var PI = 3.14;
     var theta_scale = 0.03;
-    var i = 0;
-    var j = 0;
-    var k = 0;
+
+    var numberOfObjectsFront = 0;
     for (var theta = 0; theta < 2 * PI; theta += theta_scale) {
         var x = 3 * Math.cos(theta);
         var y = 3 * Math.sin(theta);
-        OBJECTS[i] = new THREE.Mesh(geometry, materialObj);
-        OBJECTS[i].position.x = x;
-        OBJECTS[i].position.z = 0;
-        OBJECTS[i].position.y = y;
-        OBJECTS[i].rotation.z = theta;
-        SCENE.add(OBJECTS[i]);
-        i++;
+        OBJECTS_FRONT[numberOfObjectsFront] = new THREE.Mesh(geometryObjFront, materialObj);
+        OBJECTS_FRONT[numberOfObjectsFront].position.x = x;
+        OBJECTS_FRONT[numberOfObjectsFront].position.z = 0;
+        OBJECTS_FRONT[numberOfObjectsFront].position.y = y;
+        OBJECTS_FRONT[numberOfObjectsFront].rotation.z = theta;
+        SCENE.add(OBJECTS_FRONT[numberOfObjectsFront]);
+        numberOfObjectsFront++;
     }
 
+    var numberOfObjectsMid = 0;
     for (var theta = 0; theta < 2 * PI; theta += theta_scale) {
-        var x2 = 2 * Math.cos(theta);
-        var y2 = 2 * Math.sin(theta);
-        CIRCLEOBJECTS1[j] = new THREE.Mesh(geometry2, materialObj);
-        CIRCLEOBJECTS1[j].position.x = x2;
-        CIRCLEOBJECTS1[j].position.z = -3;
-        CIRCLEOBJECTS1[j].position.y = y2;
-        CIRCLEOBJECTS1[j].rotation.z = theta;
+        var x = 2 * Math.cos(theta);
+        var y = 2 * Math.sin(theta);
+        OBJECTS_MID[numberOfObjectsMid] = new THREE.Mesh(geometryObjMid, materialObj);
+        OBJECTS_MID[numberOfObjectsMid].position.x = x;
+        OBJECTS_MID[numberOfObjectsMid].position.z = -3;
+        OBJECTS_MID[numberOfObjectsMid].position.y = y;
+        OBJECTS_MID[numberOfObjectsMid].rotation.z = theta;
 
-        SCENE.add(CIRCLEOBJECTS1[j]);
-        j++;
+        SCENE.add(OBJECTS_MID[numberOfObjectsMid]);
+        numberOfObjectsMid++;
     }
+
+    var numberOfObjectsBack = 0;
     for (var theta = 0; theta < 2 * PI; theta += theta_scale) {
-        var x3 = 1 * Math.cos(theta);
-        var y3 = 1 * Math.sin(theta);
-        CIRCLEOBJECTS2[k] = new THREE.Mesh(geometry3, materialObj);
-        CIRCLEOBJECTS2[k].position.x = x3;
-        CIRCLEOBJECTS2[k].position.z = -5;
-        CIRCLEOBJECTS2[k].position.y = y3;
-        CIRCLEOBJECTS2[k].rotation.z = theta;
+        var x = 1 * Math.cos(theta);
+        var y = 1 * Math.sin(theta);
+        OBJECTS_BACK[numberOfObjectsBack] = new THREE.Mesh(geometryObjBack, materialObj);
+        OBJECTS_BACK[numberOfObjectsBack].position.x = x;
+        OBJECTS_BACK[numberOfObjectsBack].position.z = -5;
+        OBJECTS_BACK[numberOfObjectsBack].position.y = y;
+        OBJECTS_BACK[numberOfObjectsBack].rotation.z = theta;
 
-        SCENE.add(CIRCLEOBJECTS2[k]);
-        k++;
+        SCENE.add(OBJECTS_BACK[numberOfObjectsBack]);
+        numberOfObjectsBack++;
     }
-    //
 
+    // SPHERE  + "SKYBOX"
+    OBJ_SPHERE = new THREE.Mesh(geometrySphere, materialObj);
+    OBJ_SKYBOX = new THREE.Mesh(geometrySkybox, materialSkybox);
 
-    SCENE.add(OBJECT2);
-    //SCENE.add(SKYBOX);
-
-
-
-
-
-
-
-
+    SCENE.add(OBJ_SPHERE);
+    SCENE.add(OBJ_SKYBOX);
 
     // MOUSE
     CANVAS.addEventListener('mousemove', function (evt) {
@@ -389,31 +334,20 @@ document.addEventListener('keydown', function (evt) {
 
 //////////////////////////////////////////////////////////////////////////////////// UPDATE (SOURIS ET LOGIC)
 function update() {
-
-}
-
-//////////////////////////////////////////////////////////////////////////////////// RENDER
-function render() {
-    // TODO: Faire un update avec une fréquence fixe
-    update();
-
-    // RENDER
-    requestAnimationFrame(render);
-
-    for (var i = 0; i < OBJECTS.length; i++) {
-        //OBJECTS[i].position.x += move;
-
+    for (var i = 0; i < OBJECTS_FRONT.length; i++) {
+        //OBJECTS_FRONT[i].position.x += move;
     }
 
-    for (var i = 0; i < OBJECTS.length; i += 2) {
-        //OBJECTS[i].position.x += move;
-        OBJECTS[i].scale.x += scaleValue;
+    for (var i = 0; i < OBJECTS_FRONT.length; i += 2) {
+        //OBJECTS_FRONT[i].position.x += move;
+        OBJECTS_FRONT[i].scale.x += scaleValue;
     }
-    for (var i = 1; i < OBJECTS.length; i += 5) {
-        //OBJECTS[i].position.x += move;
-        OBJECTS[i].scale.x += scaleValue + anotherScaleValue;
-        CIRCLEOBJECTS1[i].scale.z += scaleValue + anotherScaleValue * 9;
-        CIRCLEOBJECTS2[i].scale.y += scaleValue + anotherAnotherScaleValue * 3;
+
+    for (var i = 1; i < OBJECTS_FRONT.length; i += 5) {
+        //OBJECTS_FRONT[i].position.x += move;
+        OBJECTS_FRONT[i].scale.x += scaleValue + anotherScaleValue;
+        OBJECTS_MID[i].scale.z += scaleValue + anotherScaleValue * 9;
+        OBJECTS_BACK[i].scale.y += scaleValue + anotherAnotherScaleValue * 3;
     }
 
     actualValue += move;
@@ -421,7 +355,6 @@ function render() {
 
     if (actualValue > MaxValue || actualValue < MinValue) {
         move *= -1;
-
     }
 
     if (actualScale > MaxValue || actualScale < MinValue) {
@@ -432,7 +365,6 @@ function render() {
         anotherScaleValue *= -1;
         anotherAnotherScaleValue *= -1
     }
-
 
     //Modification des couleures en fonction du son
     if (mySoundAnalyser) {
@@ -445,14 +377,16 @@ function render() {
             uniforms.colors.value.x = 0.0;
         }
 
-
-
-        OBJECT2.scale.x = yolo / 100;
-        OBJECT2.scale.y = yolo / 100;
-        OBJECT2.scale.z = yolo / 100;
+        OBJ_SPHERE.scale.x = yolo / 100;
+        OBJ_SPHERE.scale.y = yolo / 100;
+        OBJ_SPHERE.scale.z = yolo / 100;
     }
+}
 
-
+//////////////////////////////////////////////////////////////////////////////////// RENDER
+function run() {
+    update();
+    requestAnimationFrame(run);
     RENDERER.render(SCENE, CAMERA);
 }
 
@@ -460,5 +394,5 @@ function render() {
 //////////////////////////////////////////////////////////////////////////////////// Le début quoi
 $(document).ready(function () {
     init();
-    render();
+    run();
 });
