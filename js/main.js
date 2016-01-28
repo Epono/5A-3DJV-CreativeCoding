10b30d3571e2391349f18dab167c3d1a77443469
Epono <guillaume.ambrois@gmail.com>
2016-01-28 12:51:20 +0100

    Shaders + orbitControls + Fix p5 + resize

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 // Pour les FPS
-javascript:(function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();stats.domElement.style.cssText='position:fixed;left:0;top:0;z-index:10000';document.body.appendChild(stats.domElement);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='//rawgit.com/mrdoob/stats.js/master/build/stats.min.js';document.head.appendChild(script);})()
+javascript: (function () {
+    var script = document.createElement('script');
+    script.onload = function () {
+        var stats = new Stats();
+        stats.domElement.style.cssText = 'position:fixed;left:0;top:0;z-index:10000';
+        document.body.appendChild(stats.domElement);
+        requestAnimationFrame(function loop() {
+            stats.update();
+            requestAnimationFrame(loop)
+        });
+    };
+    script.src = '//rawgit.com/mrdoob/stats.js/master/build/stats.min.js';
+    document.head.appendChild(script);
+})()
 
 //////////////////////////////////////////////////////////////////////////////////// LES VARIABLES GLOBALES C'EST LE BIEN
 var SCENE = null;
@@ -7,6 +20,7 @@ var CAMERA = null;
 var RENDERER = null;
 var OBJECT = null;
 var CANVAS = null;
+var CONTROLS = null;
 
 var mouseSpeed = 0.02;
 var mouse = {
@@ -23,14 +37,18 @@ var mouse = {
     rightDown: false
 }
 
+// Pour faire une simulation unique à chaque lancement (en plus du nombreMagique)
+var startTime = Date.now();
+
 var music = null;
 var sounds = {};
 
 //////////////////////////////////////////////////////////////////////////////////// TRUCS PROCESSING
 function preload() {
-    music = loadSound('assets/music/titanium.mp3');
+    music = loadSound('assets/music/titanium.mp3', function () {
+        console.log('Music loaded !')
+    });
     music.setVolume(0.1);
-
 
     sounds['A'] = loadSound('assets/sounds/A - AAAAH.mp3');
     sounds['A'].setVolume(0.1);
@@ -55,12 +73,16 @@ function preload() {
 
     sounds['S'] = loadSound('assets/sounds/S - You got completely shitfaced.wav');
     sounds['S'].setVolume(0.2);
+
+    // Pour cacher le cnnvas processing
+    $('#p5_loading').hide();
+    $('#defaultCanvas0').hide();
 }
 
 function setup() {
-    // PROCESSING
+    // Pour le chargement de la musique
 }
-
+var date = Date.now();
 //////////////////////////////////////////////////////////////////////////////////// INIT SCENE
 function init() {
     SCENE = new THREE.Scene();
@@ -72,9 +94,21 @@ function init() {
     document.body.appendChild(RENDERER.domElement);
 
     var geometry = new THREE.BoxGeometry(1, 1, 1);
+
+    // Material de base
     var material = new THREE.MeshBasicMaterial({
         color: 0x00ff00
     });
+
+    // Material avec des shaders
+    var vShader = document.getElementById('shader-vs');
+    var fShader = document.getElementById('shader-fs');
+    material = new THREE.ShaderMaterial({
+
+        vertexShader: vShader.textContent,
+        fragmentShader: fShader.textContent
+    });
+
     OBJECT = new THREE.Mesh(geometry, material);
 
     SCENE.add(OBJECT);
@@ -82,6 +116,13 @@ function init() {
     CAMERA.position.z = 5;
 
     CANVAS = RENDERER.domElement;
+
+    SCENE.fog = new THREE.FogExp2(0xcccccc, 0.002);
+    RENDERER.setClearColor(SCENE.fog.color);
+
+    CONTROLS = new THREE.OrbitControls(CAMERA, RENDERER.domElement);
+    CONTROLS.enableDamping = true;
+    CONTROLS.dampingFactor = 0.25;
 
     // MOUSE
     CANVAS.addEventListener('mousemove', function (evt) {
@@ -129,6 +170,13 @@ function init() {
             break;
         }
     }, false);
+
+    window.addEventListener('resize', function () {
+        CAMERA.aspect = window.innerWidth / window.innerHeight;
+        CAMERA.updateProjectionMatrix();
+
+        RENDERER.setSize(window.innerWidth, window.innerHeight);
+    }, false);
 }
 
 //////////////////////////////////////////////////////////////////////////////////// "UPDATE" CLAVIER
@@ -145,10 +193,10 @@ document.addEventListener('keydown', function (evt) {
 
         var nombreMagique = Math.pow(2, evt.keyCode - 64);
 
-        Math.seedrandom(nombreMagique);
+        Math.seedrandom(nombreMagique + startTime);
 
-        var radius = 10;
-        var numberOfObjects = 10;
+        var radius = 20;
+        var numberOfObjects = 40;
 
         var objects = [];
 
@@ -157,9 +205,11 @@ document.addEventListener('keydown', function (evt) {
 
             obj.position.x = (Math.random() - 0.5) * radius;
             obj.position.y = (Math.random() - 0.5) * radius;
+            obj.position.z = (Math.random() - 0.5) * radius;
 
             obj.scale.x = Math.random();
             obj.scale.y = Math.random();
+            obj.scale.z = Math.random();
 
             obj.rotation.x = Math.random();
             obj.rotation.y = Math.random();
@@ -181,27 +231,20 @@ document.addEventListener('keydown', function (evt) {
         }, 200);
     }
 
-    console.log(evt.keyCode);
     if (evt.keyCode === 101) {
-        if (music.isPlaying()) {
-            music.stop();
-        } else {
-            music.loop();
+        if (music.isLoaded()) {
+            if (music.isPlaying()) {
+                music.stop();
+            } else {
+                music.loop();
+            }
         }
     }
 });
 
 //////////////////////////////////////////////////////////////////////////////////// UPDATE (SOURIS ET LOGIC)
 function update() {
-    // MOUSE
-    if (mouse.leftDown) {
-        OBJECT.position.x += mouse.deltaPos.x * mouseSpeed;
-        OBJECT.position.y -= mouse.deltaPos.y * mouseSpeed;
-    }
 
-    // OTHER
-    OBJECT.rotation.x += 0.05;
-    OBJECT.rotation.y += 0.05;
 }
 
 //////////////////////////////////////////////////////////////////////////////////// RENDER
